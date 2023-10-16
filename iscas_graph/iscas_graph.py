39893f04d2fdb01ccacc9330d1d57d63cde21b96
sackey21 <sackey.sg@gmail.com>
2023-10-16 13:47:15 +0900

    ゲート命令抽出処理追加

--- iscas_graph/iscas_graph.py
+++ iscas_graph/iscas_graph.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 import re
+import networkx as nx
 
 
 def main(path):
@@ -12,12 +13,15 @@ def main(path):
     # データ整形
     inst_list = re.sub('//.*\n', '', data).replace('\n', '').split(';')
 
-    # input宣言命令のリスト
+    # 命令をリストに格納
+    # input命令のリスト
     inst_input = []
-    # output宣言命令のリスト
+    # output命令のリスト
     inst_output = []
-    # wire宣言命令のリスト
+    # wire命令のリスト
     inst_wire = []
+    # ゲート接続命令のリスト
+    inst_primitive_gate = []
 
     for inst in inst_list:
         # input
@@ -29,8 +33,14 @@ def main(path):
         # wire
         elif 'wire ' in inst:
             inst_wire.append(inst)
-    
-        
+        # primitive gate
+        elif re.compile("and |nand |or |nor |xor |xnor |buf |not ").match(inst):
+            inst_primitive_gate.append(inst)
+
+    # print(inst_input)
+    # print(inst_output)
+    # print(inst_wire)
+    # print(inst_primitive_gate)
 
 if __name__ == '__main__':
     args = sys.argv
