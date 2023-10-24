1e2b4d6f28b5ee0df71f7bcf538417e11b5bbce9
sackey21 <sackey.sg@gmail.com>
2023-10-24 16:07:18 +0900

    とりあえずグラフとしてゲート名と接続関係だけは出力できてるんとちゃいますかね～

--- iscas_graph/iscas_graph.py
+++ iscas_graph/iscas_graph.py
@@ -4,6 +4,9 @@ import pathlib
 import re
 import networkx as nx  # 直置きpython3に直インストールしてます
 
+
+import matplotlib.pyplot as plt
+
 from Gate import Gate
 
 
@@ -41,7 +44,7 @@ def main(path):
             insts_primitive_gate.append(inst)
 
     # グラフで代替可能なのでこれいらんかもしれへん。
-    circuit_gates: List[Gate()] = [] # グラフ作成
+    circuit_gates: List[Gate()] = []  # グラフ作成
     circuit_graph = nx.Graph()
 
     # 命令から必要情報の抽出
@@ -53,14 +56,16 @@ def main(path):
         gate_type = gate_def_info.split()[0]
 
         # 入出力指定部分の取り出し
-        gate_connect_info = inst.split('(')[-1].replace(')', '').replace(' ', '').split(',')
+        gate_connect_info = inst.split(
+            '(')[-1].replace(')', '').replace(' ', '').split(',')
 
         output = gate_connect_info.pop(0)
         input = gate_connect_info
 
         # ゲートの登録
         circuit_gates.append(Gate(gate_name, gate_type, output, input))
-        circuit_graph.add_node(gate_name, type = gate_type, input = input, output = output)
+        circuit_graph.add_node(gate_name, type=gate_type,
+                               input=input, output=output)
 
     # ワイヤの抽出
     wires: List[str] = []
@@ -74,7 +79,6 @@ def main(path):
     # ゲートとゲートの接続関係をエッジとして追加
     # エッジの追加方策：ワイヤのリストをループで回して、inputでそのワイヤを持ってるゲートとoutputでそのワイヤを持ってるゲートを検出して、エッジを追加する。
     # エッジに遷移確率を重みとして追加
-
     print(dict(nx.get_node_attributes(circuit_graph, 'input')))
     for wire in wires:
         input_gate: List[str] = [i for i, value in dict(
@@ -85,6 +89,10 @@ def main(path):
             for output in output_gate:
                 circuit_graph.add_edges_from([(input, output)], label=wire)
     print(circuit_graph)
+
+    # ネットワーク図描写
+    nx.draw_networkx(circuit_graph)
+    plt.savefig('test')
 
 if __name__ == '__main__':
     args = sys.argv
