bdb3e9bef41db637794decc6af10e1ec7a510e77
sackey21 <sackey.sg@gmail.com>
2023-10-24 15:35:13 +0900

    グラフ実装完了。あと変数名を複数のところは対応して変更しました。フォーマッタなんかキモくなってる

--- iscas_graph/iscas_graph.py
+++ iscas_graph/iscas_graph.py
@@ -2,9 +2,10 @@ from typing import List
 import sys
 import pathlib
 import re
-import networkx as nx # 直置きpython3に直インストールしてます
+import networkx as nx  # 直置きpython3に直インストールしてます
 
 from Gate import Gate
+
 
 def main(path):
     f = open(path, 'r', encoding='UTF-8')
@@ -13,67 +14,77 @@ def main(path):
     f.close()
 
     # データ整形
-    inst_list = re.sub('//.*\n', '', data).replace('\n', '').split(';')
+    insts_list = re.sub('//.*\n', '', data).replace('\n', '').split(';')
 
     # 命令をリストに格納
     # input命令のリスト
-    inst_input: List[str] = []
+    insts_input: List[str] = []
     # output命令のリスト
-    inst_output: List[str] = []
+    insts_output: List[str] = []
     # wire命令のリスト
-    inst_wire: List[str] = []
+    insts_wire: List[str] = []
     # ゲート接続命令のリスト
-    inst_primitive_gate: List[str] = []
+    insts_primitive_gate: List[str] = []
 
-    for inst in inst_list:
+    for inst in insts_list:
         # input
         if 'input ' in inst:
-            inst_input.append(inst)
+            insts_input.append(inst)
         # output
         elif 'output ' in inst:
-            inst_output.append(inst)
+            insts_output.append(inst)
         # wire
         elif 'wire ' in inst:
-            inst_wire.append(inst)
+            insts_wire.append(inst)
         # primitive gate
         elif re.compile("and |nand |or |nor |xor |xnor |buf |not ").match(inst):
-            inst_primitive_gate.append(inst)
-    
+            insts_primitive_gate.append(inst)
+
     # グラフで代替可能なのでこれいらんかもしれへん。
-    circuit_gates : List[Gate()] = []
-     # グラフ作成
+    circuit_gates: List[Gate()] = [] # グラフ作成
     circuit_graph = nx.Graph()
 
     # 命令から必要情報の抽出
-    for inst in inst_primitive_gate:
-        
+    for inst in insts_primitive_gate:
+
         # 命令部分の取り出し
         gate_def_info = inst.split('(')[0]
-  
         gate_name = gate_def_info.split()[-1]
         gate_type = gate_def_info.split()[0]
-              
+
         # 入出力指定部分の取り出し
         gate_connect_info = inst.split('(')[-1].replace(')', '').replace(' ', '').split(',')
-        
+
         output = gate_connect_info.pop(0)
         input = gate_connect_info
-        
+
         # ゲートの登録
         circuit_gates.append(Gate(gate_name, gate_type, output, input))
         circuit_graph.add_node(gate_name, type = gate_type, input = input, output = output)
-    
-    for a in circuit_gates:
-        print(vars(a))
-        
+
+    # ワイヤの抽出
+    wires: List[str] = []
+    for inst in insts_wire:
+        for wire in inst.replace('wire', '').replace(' ', '').split(','):
+            wires.append(wire)
+
+    # print(wires)
 
     # エッジについての実装
     # ゲートとゲートの接続関係をエッジとして追加
     # エッジの追加方策：ワイヤのリストをループで回して、inputでそのワイヤを持ってるゲートとoutputでそのワイヤを持ってるゲートを検出して、エッジを追加する。
-    
-    
     # エッジに遷移確率を重みとして追加
 
+    print(dict(nx.get_node_attributes(circuit_graph, 'input')))
+    for wire in wires:
+        input_gate: List[str] = [i for i, value in dict(
+            nx.get_node_attributes(circuit_graph, 'input')).items() if wire in value]
+        output_gate: List[str] = [i for i, value in dict(
+            nx.get_node_attributes(circuit_graph, 'output')).items() if wire in value]
+        for input in input_gate:
+            for output in output_gate:
+                circuit_graph.add_edges_from([(input, output)], label=wire)
+    print(circuit_graph)
 
 if __name__ == '__main__':
     args = sys.argv
