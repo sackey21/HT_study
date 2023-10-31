57a1098db5efb5c4d20d058efd06af8de5a85fe2
sackey21 <sackey.sg@gmail.com>
2023-10-31 16:03:59 +0900

    基本のグラフでけた！c17.vはうごいたけどそれ以外はゲート多くて動かんぽい！おわり！

--- iscas_graph/iscas_graph.py
+++ iscas_graph/iscas_graph.py
@@ -9,6 +9,9 @@ from Gate import Gate
 
 
 def main(path):
+
+    circuit_graph = nx.Graph()
+
     f = open(path, 'r', encoding='UTF-8')
     data = f.read()
 
@@ -41,9 +44,19 @@ def main(path):
         elif re.compile("and |nand |or |nor |xor |xnor |buf |not ").match(inst):
             insts_primitive_gate.append(inst)
 
-    circuit_graph = nx.Graph()
+    # 入力信号線抽出
+    primary_input_wire: List[str] = []
+    for inst in insts_input:
+        for input_wire in inst.replace('input ', '').split(','):
+            primary_input_wire.append(input_wire)
 
-    # 命令から必要情報の抽出
+    # 出力信号線抽出
+    primary_output_wire: List[str] = []
+    for inst in insts_output:
+        for output_wire in inst.replace('output ', '').split(','):
+            primary_output_wire.append(output_wire)
+
+    # ゲート関連の命令から必要情報の抽出
     for inst in insts_primitive_gate:
 
         # 命令部分の取り出し
@@ -57,12 +70,29 @@ def main(path):
 
         output = gate_connect_info.pop(0)
         input = gate_connect_info
-
         # ゲートの登録
         circuit_graph.add_node(gate_name, type=gate_type,
                                input=input, output=output)
 
-    print(circuit_graph.nodes)
+    # 外からの入力線を表すエッジとノードの設定、入力先ゲートと接続
+    input_num: int = 0
+    for wire in primary_input_wire:
+        input_num += 1
+        primary_input: str = "input" + str(input_num)
+        circuit_graph.add_node(primary_input)
+        for input in [i for i, value in dict(
+                nx.get_node_attributes(circuit_graph, 'input')).items() if wire in value]:
+            circuit_graph.add_edge(primary_input, input, label=wire)
+
+    # 外へ出力する信号線を表すエッジとノードの設定、出力元ゲートと接続
+    output_num: int = 0
+    for wire in primary_output_wire:
+        output_num += 1
+        primary_output: str = "output" + str(output_num)
+        circuit_graph.add_node(primary_output)
+        for output in [i for i, value in dict(
+                nx.get_node_attributes(circuit_graph, 'output')).items() if wire in value]:
+            circuit_graph.add_edge(primary_output, output, label=wire)
 
     # ワイヤの抽出
     wires: List[str] = []
@@ -70,9 +100,7 @@ def main(path):
         for wire in inst.replace('wire', '').replace(' ', '').split(','):
             wires.append(wire)
 
-    # print(wires)
-
-    # エッジ追加
+    # ワイヤをエッジとして追加
     for wire in wires:
         print(wire)
         input_gate: List[str] = [i for i, value in dict(
@@ -83,15 +111,18 @@ def main(path):
             for output in output_gate:
                 circuit_graph.add_edge(input, output, label=wire)
 
+    # グラフ描写部。
     # エッジのラベルを取得
     edge_labels = {edge: circuit_graph[edge[0]][edge[1]]
                    ["label"] for edge in circuit_graph.edges()}
     print(edge_labels)
 
-    # ネットワーク図出力# ネットワーク図の描画
+    # ネットワーク図出力
     pos = nx.spring_layout(circuit_graph)
-    nx.draw(circuit_graph, pos, with_labels=True, node_size=500, node_color="skyblue", font_size=10, font_color="black")
-    nx.draw_networkx_edge_labels(circuit_graph, pos, edge_labels=edge_labels, font_color="red")
+    nx.draw(circuit_graph, pos, with_labels=True, node_size=500,
+            node_color="skyblue", font_size=10, font_color="black")
+    nx.draw_networkx_edge_labels(
+        circuit_graph, pos, edge_labels=edge_labels, font_color="red")
 
     plt.savefig('test')
 
